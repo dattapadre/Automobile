7ed74f2b46a17acf9847493c6d580f054e9a35b3
shubham9756 <skarad819@gmail.com>
2025-08-12 11:51:45 +0530

    conn

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql')
 var util = require('util')
 
 var conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'shrihari'
+    host:'bn8txrkcfp43vajmsopo-mysql.services.clever-cloud.com',
+    user:'urvn9rqiq1nki9wd',
+    password:'urvn9rqiq1nki9wd',
+    database:'bn8txrkcfp43vajmsopo'
 })
 
 var exe = util.promisify(conn.query).bind(conn)
