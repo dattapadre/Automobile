db580e2cedccc8048ec16a21857d620fe9b6ff13
shubham9756 <skarad819@gmail.com>
2025-08-12 10:16:26 +0530

    lanch

---
+++ index.js
@@ -0,0 +1,22 @@
+var express =require('express')
+var bodyparser =require('body-parser')
+var upload = require('express-fileupload')
+var session = require('express-session')
+var admin_router = require('./router/admin')
+var user_router = require('./router/user')
+const cookieParser = require('cookie-parser');
+var app =express()
+app.use(cookieParser());
+
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static('public/'))
+app.use(upload())
+app.use(session({
+    secret:'qwertyi',
+    resave:true,
+    saveUninitialized:true
+}))
+app.use('/',user_router)
+app.use('/admin',admin_router)
+
+app.listen(1000)
