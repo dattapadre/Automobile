e9e31919131c44d5ac5605d19dc0ea1ffda9ce41
sadpathan <sadikpathan044@gmail.com>
2025-08-12 11:00:52 +0530

    ll

--- router/admin.js
+++ router/admin.js
@@ -3,6 +3,9 @@ var router = express.Router()
 
 router.get("/",function(req,res){
     res.render("admin/home.ejs")
-})
+});
+router.get("/vehicles", function (req,res){
+    res.render("admin/vehicles.ejs")
+});
 
 module.exports = router;
